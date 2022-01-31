83aaa2007b00b3048b0384502a066f7856f14702
erubazo <98761502+erubazo@users.noreply.github.com>
2022-01-31 17:55:26 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,63 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+       
+        
+        gallery_fun: function () {
+            /*====================================
+                 FOR IMAGE/GALLERY POPUP
+            ======================================*/
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            /*====================================
+            FOR IMAGE/GALLERY FILTER
+            ======================================*/
+
+            // MixItUp plugin
+            // http://mixitup.io
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+      
+      
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.gallery_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
